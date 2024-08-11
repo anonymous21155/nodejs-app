df22640f111a9789695a1da41e645a516d302e79
anonymous21155 <gokul.dev@10xds.com>
2024-08-11 19:32:38 +0530

    req handle

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 1500;
 
-
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>');
 });
